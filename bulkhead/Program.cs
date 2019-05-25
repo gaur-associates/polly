806b41e1040a698669c266ffc77e9a8ea87cab0f
yogesh gaur <yogi7.hills@gmail.com>
2019-05-25 09:59:53 -0500

    initial

---
+++ bulkhead/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using Polly;
+using Polly.Bulkhead;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            setup();
+
+            List<Task> taskList = new List<Task>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                taskList.Add(Task.Run(() => fetch(i)));
+            }
+            Task.WaitAll(taskList.ToArray());
+
+        }
+        static void setup()
+        {
+            bulkheadIsolationPolicy = Policy
+                          .BulkheadAsync<HttpResponseMessage>(2, 2, OnBulkheadRejectedAsync);
+        }
+        static async Task fetch(int id)
+        {
+            try
+            {
+                LogBulkheadInfo();
+                HttpResponseMessage response = await bulkheadIsolationPolicy.ExecuteAsync(
+                               () => httpClient.GetAsync(requestEndpoint + "/" + id));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var post = JsonConvert.DeserializeObject<Post>(json);
+                    Console.WriteLine(post.title);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+
+            }
+        }
+
+        static string requestEndpoint = "https://jsonplaceholder.typicode.com/posts";
+        static HttpClient httpClient = new HttpClient();
+        static BulkheadPolicy<HttpResponseMessage> bulkheadIsolationPolicy;
+
+
+        static Task OnBulkheadRejectedAsync(Context context)
+        {
+            Console.WriteLine($"PollyDemo OnBulkheadRejectedAsync Executed");
+            return Task.CompletedTask;
+        }
+        static void LogBulkheadInfo()
+        {
+
+            Console.WriteLine($"BulkheadAvailableCount " +
+                                               $"{bulkheadIsolationPolicy.BulkheadAvailableCount}");
+            Console.WriteLine($"QueueAvailableCount " +
+                                               $"{bulkheadIsolationPolicy.QueueAvailableCount}");
+        }
+    }
+}
---
+++ retryCache/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using Polly;
+using Polly.Caching;
+using Polly.Caching.MemoryCache;
+using Polly.Retry;
+
+using Polly.Wrap;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            setup();
+            fetch(1).GetAwaiter().GetResult();
+            fetch(1).GetAwaiter().GetResult();
+        }
+
+        static async Task fetch(int id)
+        {
+            Context policyExecutionContext = new Context($"GetById-{id}");
+
+            Console.WriteLine("making http get call");
+            HttpResponseMessage response = await policyWrap.ExecuteAsync(
+                           (ctx) => httpClient.GetAsync(requestEndpoint + "/" + id), policyExecutionContext);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var post = JsonConvert.DeserializeObject<Post>(json);
+                Console.WriteLine(post.title);
+            }
+        }
+
+        static string requestEndpoint = "https://jsonplaceholder.typicode.com/postsa";
+        static HttpClient httpClient = new HttpClient();
+        static IAsyncPolicy<HttpResponseMessage> retryPolicy;
+        static PolicyWrap<HttpResponseMessage> policyWrap;
+        static IAsyncPolicy<HttpResponseMessage> cachePolicy;
+        static void setup()
+        {
+            retryPolicy = Policy
+                           .Handle<Exception>()
+                           .OrResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                           .RetryAsync(2, (ex, retryCnt) =>
+                           {
+                               Console.WriteLine($"Retry count {retryCnt}");
+                           });
+
+            var myCache = new MemoryCache(new MemoryCacheOptions());
+            MemoryCacheProvider memoryCacheProvider
+               = new MemoryCacheProvider(myCache);
+
+            cachePolicy =
+                Policy.CacheAsync<HttpResponseMessage>(memoryCacheProvider, TimeSpan.FromMinutes(5));
+
+
+            policyWrap = Policy.WrapAsync<HttpResponseMessage>(cachePolicy, retryPolicy);
+        }
+    }
+}
